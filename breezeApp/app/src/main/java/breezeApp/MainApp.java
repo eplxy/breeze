26d99c3b355cfb2a50670eb2e9b7e492d644b8a2
Steven <2186084@edu.vaniercollege.qc.ca>
2023-07-08 15:53:55 -0400

    Post Toronto Trip

--- breezeApp/app/src/main/java/breezeApp/MainApp.java
+++ breezeApp/app/src/main/java/breezeApp/MainApp.java
@@ -8,19 +8,35 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+    //test start using ColorRecognitionController
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainMenu.fxml"));
-        MainMenuController mmc = new MainMenuController();
-        loader.setController(mmc);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/colorRecognitionMenu.fxml"));
+        ImageAdjustMenuController crc = new ImageAdjustMenuController(stage);
+        loader.setController(crc);
         BorderPane root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Breeze");
+        stage.setTitle("Color Recognition Test");
         stage.sizeToScene();
         stage.show();
+
+        crc.initialize();
+
     }
 
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainMenu.fxml"));
+//        MainMenuController mmc = new MainMenuController();
+//        loader.setController(mmc);
+//        BorderPane root = loader.load();
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.setTitle("Breeze");
+//        stage.sizeToScene();
+//        stage.show();
+//    }
     public static void main(String[] args) {
         launch(args);
     }
