ddee859a88765b4b43b65cf0021483d53d17708d
eplxy <steven.dvlam@gmail.com>
2024-06-10 20:52:16 -0400

    End of June 10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from "./pages/About.js";
 import Header from "./components/Header.jsx";
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
 
   return (
     <Router>
-    <div className="Main">
-    
-      <Header />
-      
-        <h1>{!data ? "Loading..." : data}</h1>
-        
-      <Routes>
-        <Route path="/about" element={<About />} /> {/* Add this line */}
-      </Routes>
-    </div>
-  </Router>
+      <div className="Main">
+
+        <Header />
+
+        {/* <h1>{!data ? "Loading..." : data}</h1> */}
+
+        <Routes>
+          <Route path="/about" element={<About />} /> {/* Add this line */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
